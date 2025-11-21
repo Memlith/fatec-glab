e37dfaf249d69ec4e10456349f628ee6c6cc53f9
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-21 11:08:03 -0300

    feat: add Spring Doc - OpenApi, on project

--- backend/src/main/java/com/fatec/glab/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/fatec/glab/exception/GlobalExceptionHandler.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IdNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleIdNotFoundException(IdNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleNoResourceFoundException(NoResourceFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(e.getMessage()));
     }
 
