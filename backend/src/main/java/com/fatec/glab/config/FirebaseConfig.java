49cc2c670dc3b20bcb53ec6cfe62db0c0bed4478
Memlith <iamcaiosimonassi@gmail.com>
2026-06-02 17:26:11 -0300

    feat: update Firebase configuration to use encoded credentials

--- backend/src/main/java/com/fatec/glab/config/FirebaseConfig.java
+++ backend/src/main/java/com/fatec/glab/config/FirebaseConfig.java
@@ -4,45 +4,37 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.base64}")
+    private String firebaseCredentialsBase64;
+
     @PostConstruct
     public void init() {
-
         try {
+            byte[] decodedBytes = Base64.getDecoder().decode(firebaseCredentialsBase64);
+            
+            InputStream serviceAccount = new ByteArrayInputStream(decodedBytes);
 
-            InputStream serviceAccount =
-                    new ClassPathResource(
-                            "firebase-service-account.json"
-                    ).getInputStream();
-
-            FirebaseOptions options =
-                    FirebaseOptions.builder()
-                            .setCredentials(
-                                    GoogleCredentials
-                                            .fromStream(serviceAccount)
-                            )
-                            .build();
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
-
                 FirebaseApp.initializeApp(options);
             }
-
             System.out.println("Firebase initialized!");
 
         } catch (Exception e) {
-
-            throw new RuntimeException(
-                    "Erro ao inicializar Firebase",
-                    e
-            );
+            throw new RuntimeException("Erro ao inicializar Firebase", e);
         }
     }
 }
